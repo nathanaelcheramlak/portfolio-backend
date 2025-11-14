37d82cdb0b445f88c34d521ca054ae9c0eb6de8e
nathanaelcheramlak <nathanaelcheramlak7@gmail.com>
2025-11-14 18:05:06 +0300

    refactor: removed unnecessary comments

--- utils.py
+++ utils.py
@@ -12,11 +12,3 @@ def format_message(data):
 
     message = f"From: {name} \nEmail: {email} \nSubject: {subject} \n \n{content} \n\nDate: {curr_date}"
     return message
-
-# data = {
-#     'name': 'Yonas Getachew', 
-#     'email': 'yonibord@mail.com',
-#     'subject': 'Request for a Job',
-#     'content': 'Lorem Ipsum for the rest of the day and you have to finish alot of tasks so be expreienced.'
-# }
-# print(format_message(data))
